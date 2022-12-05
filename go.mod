753436a3e35644b6ee2883e922e13a9c0a253080
Will Arroyo <warroyo7199008@gmail.com>
2022-12-05 14:45:14 -0700

    add ability to use service bindings

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/nebhale/client-go v1.0.0
 )
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -5,14 +5,47 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/nebhale/client-go/bindings"
 )
 
 func ConnectToDB(init bool) *sql.DB {
 	const file string = "companies.db"
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
+	var dbUser = ""
+	var dbPass = ""
+	var dbName = ""
+	var dbHost = ""
+	var ok = false
+
+	b := bindings.FromServiceBindingRoot()
+	b = bindings.Filter(b, "mysql")
+	if len(b) != 1 {
+		_, _ = fmt.Fprintf(os.Stderr, "Incorrect number of MYSQL drivers: %d\n", len(b))
+		log.Println("no service bindings present falling back to ENV vars")
+		dbUser = os.Getenv("DB_USER")
+		dbPass = os.Getenv("DB_PASSWORD")
+		dbName = os.Getenv("DB_NAME")
+		dbHost = os.Getenv("DB_HOST")
+	} else {
+		log.Println("service bindings present")
+		dbName, ok = bindings.Get(b[0], "database")
+		if !ok {
+			log.Fatal("No database name in binding")
+		}
+		dbHost, ok = bindings.Get(b[0], "host")
+		if !ok {
+			log.Fatal("No host in binding")
+		}
+		dbUser, ok = bindings.Get(b[0], "username")
+		if !ok {
+			log.Fatal("No username in binding")
+		}
+		dbPass, ok = bindings.Get(b[0], "password")
+		if !ok {
+			log.Fatal("No password in binding")
+		}
+
+	}
 
 	//check if DB params are sent, if not create sqllite
 	if dbHost == "" {
